e8f87888e47fdf0015cfa549a1321a8b0a79a809
niketb2309 <niketb87@gmail.com>
2018-08-27 16:47:49 +0530

    js changes

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,17 +1,17 @@
 function validateField() {
-    event.preventDefault();
     var title = document.getElementById("noteTitle").value;
     var description = document.getElementById("noteDescription").value;
 
     if (title.length < 6 || title.length > 30) {
+        event.preventDefault();
         alert("Title size should be more than 6 and less than 30");
         document.getElementById("noteTitle").focus();
-        return false;
     }
 
     if (description.length < 6) {
+        event.preventDefault();
         alert("Description size should be more than 6");
         document.getElementById("noteDescription").focus();
-        return false;
     }
+    return true;
 }
